f9412eaea7d1ea58187b22d0ec703b97077f6c20
Emal Isuranga <emal.i@findmyfare.com>
2019-11-07 12:43:04 +0530

    second coomit

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input  } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
+})
+export class NavComponent implements  OnInit {
+
+  @Input() name: string;
+
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+
+
+
+}
